263b99ac9827cebdf6081dd350efb63748ac57d1
NickRashad <nrsternes@gmail.com>
2019-02-20 19:42:03 -0600

    Complete Bare Minimum Requirements

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, color, food, eat, job, forage, treasureChest) {
+    super();  // (age, color, food, job, forage, treasureChest);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble() {
+    this.treasureChest.push('treasure');
+  }
 };
