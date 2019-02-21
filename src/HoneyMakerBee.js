e815e92534f909761f6620d703f908c6a2e87afb
NickRashad <nrsternes@gmail.com>
2019-02-20 19:41:14 -0600

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, color, food, eat) {
+    super(age, color, food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
