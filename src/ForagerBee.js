1abb1f7163898e2e0460cfcfa5f77e68e7e0559b
NickRashad <nrsternes@gmail.com>
2019-02-20 19:41:35 -0600

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(age, color, food, eat) {
+    super(age, color, food, eat);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
